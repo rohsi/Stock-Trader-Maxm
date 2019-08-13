ded434bb67c6f483c44955583b48e69121ef66b9
Rohit Sinha <rohitsinha4826@gmail.com>
2019-08-13 16:14:47 +0530

    new

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {router} from './router/routes'
+import {store} from './store/index'
+
 
 import BootstrapVue from 'bootstrap-vue'
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
 
 new Vue({
+  el:'#app',
+
   render: h => h(App),
+  router,
+  store
+
+
 }).$mount('#app')
+
+
